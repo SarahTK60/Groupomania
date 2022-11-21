5e615ea73428a5708dda73640bf7a93f409bb143
SarahTK60 <sarah60190@gmail.com>
2022-11-21 07:23:51 +0100

    re-implement login and signup rate limiters

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authCtrl = require('../controllers/auth');
 const validPassword = require('../middleware/valid-password');
+const { loginLimiter, signUpLimiter } = require('../middleware/rate-limiter');
 
 // POST /api/auth/signup
 // Route that adds a user in database,returns user id and token
-router.post('/signup', validPassword, authCtrl.signup);
+router.post('/signup', signUpLimiter, validPassword, authCtrl.signup);
 
 // POST /api/auth/login
 // Route that checks user credentials, returns user id and token
-router.post('/login', authCtrl.login);
+router.post('/login', loginLimiter, authCtrl.login);
 
 // GET /api/auth/
 // Route that checks if a user is authenticated
